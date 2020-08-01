87899334540cc675cea9a949d79801b8b3106902
simoesrodolfo <rodolfosi.simoes@gmail.com>
2020-08-01 12:01:33 -0300

    pipeline preprocessing data

--- src/data.py
+++ src/data.py
@@ -1,6 +1,6 @@
 import os
 from glob import glob
-
+import sys
 import pandas as pd
 import numpy as np
 from pathlib import Path
@@ -62,6 +62,9 @@ def completaPaths(df_train, df_test, data_path):
     return df_train, df_test
 
 def createDirs(df_train, df_test):
+    cropped_dir = os.path.join('..', 'Data', 'lfw2_cropped')
+    if not os.path.isdir(cropped_dir):
+        os.mkdir(cropped_dir)
     df_train['path_pair_id_1_cropped'] = df_train['path_pair_id_1'].apply(lambda x: x.replace('lfw2', 'lfw2_cropped'))
     _ = df_train['path_pair_id_1_cropped'].apply(lambda x: None if os.path.isdir(os.path.split(x)[0]) 
                                                                 else os.mkdir(os.path.split(x)[0]))
@@ -90,9 +93,15 @@ def executaPreprocessamento(df_train, df_test, dim=(100, 100)):
     return df_train, df_test
 
 def geraVariavelBinaria(df_train, df_test):
-    df_train['target'] = df_train.apply(lambda x: 0 if x['pair_name_2'] is None else 1, axis=1)
-    df_test['target'] = df_test.apply(lambda x: 0 if x['pair_name_2'] is None else 1, axis=1)
+    df_train['target'] = df_train.apply(lambda x: 1 if x['pair_name_2'] is None else 0, axis=1)
+    df_test['target'] = df_test.apply(lambda x: 1 if x['pair_name_2'] is None else 0, axis=1)
     return df_train, df_test
+
+
+def gera_dataset(df_):
+    X_1 = np.array([open_img(img_1) for img_1 in df_.loc[:, 'path_pair_id_1_cropped'].values])
+    X_2 = np.array([open_img(img_2) for img_2 in df_.loc[:, 'path_pair_id_2_cropped'].values])
+    return X_1, X_2
 
 def main():
     '''
@@ -102,8 +111,13 @@ def main():
         3- Determinar e criar o diretório que as imagens serão salvas
         4- Ler cada imagem original (diretorio original) executar o pre-processamento e salvar no diretório passo 3
             4.1- Pré-processamento consiste de dois passos: VJ e resize
-        5- Construir as labels
+        5- Construir e salvar as labels, além do dataframe
+        6- Salvar X_train e X_test no path correto
     '''
+
+    args = sys.argv
+    n_rows = int(args[1])
+    n_cols = int(args[2])
     # 1
     data_folder = os.path.join('..', 'Data')
     lfw2 = Path(data_folder, 'lfw2')
@@ -117,14 +131,27 @@ def main():
     df_train, df_test = createDirs(df_train, df_test)
 
     #4.1
-    df_train, df_test = executaPreprocessamento(df_train, df_test, dim=(64,128))
+    df_train, df_test = executaPreprocessamento(df_train, df_test, dim=(n_rows,n_cols))
     #5
     df_train, df_test = geraVariavelBinaria(df_train, df_test)
-    
-    output_path = Path('Data', 'Processados')
+    y_train = df_train['target'].values
+    y_test = df_test['target'].values
+    root_output_path = Path(os.path.join(data_folder, 'Processados'))
+    root_output_path.mkdir(parents=True, exist_ok=True)
+    df_train.to_pickle(Path(root_output_path, 'df_train.pickle'))
+    df_test.to_pickle(Path(root_output_path, 'df_test.pickle'))
+    np.save(Path(root_output_path, 'y_train.npy'), y_train)
+    np.save(Path(root_output_path, 'y_test.npy'), y_test)
+
+    #6
+    output_path = Path(os.path.join(root_output_path, str(n_rows)+'_'+str(n_cols)))
     output_path.mkdir(parents=True, exist_ok=True)
-    df_train.to_pickle(Path(output_path, 'df_train.pickle'))
-    df_test.to_pickle(Path(output_path, 'df_test.pickle'))
+    X_train_1, X_train_2 = gera_dataset(df_train)
+    np.save(Path(output_path, 'X_train_1.npy'), X_train_1)
+    np.save(Path(output_path, 'X_train_2.npy'), X_train_2)
+    X_test_1, X_test_2 = gera_dataset(df_test)
+    np.save(Path(output_path, 'X_test_1.npy'), X_test_1)
+    np.save(Path(output_path, 'X_test_2.npy'), X_test_2)
 
 
 
